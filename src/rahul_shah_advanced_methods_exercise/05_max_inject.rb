0c723e9f55cb5c0c5589f096e3bb701d06c212f4
Rahul Shah <rahulshah7@outlook.com>
2019-08-25 11:22:47 +0000

    Complete advanced methods exercise

---
+++ src/rahul_shah_advanced_methods_exercise/05_max_inject.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Write a method, max_inject(arr), that accepts any amount of numbers arguments and returns
+# the largest number. Solve this using the built-in inject.
+
+def max_inject(*numbers)
+  numbers.inject do |acc, el|
+    if el > acc
+      el
+    else
+      acc
+    end
+  end
+end
+
+p max_inject(1, -4, 0, 7, 5)  # => 7
+p max_inject(30, 28, 18)      # => 30
---
+++ src/rahul_shah_advanced_methods_exercise/06_union.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Write a method, union, that accepts any number of arrays as arguments.
+# The method should return an array containing all elements of the given arrays.
+
+def union(*arrays)
+  arrays.inject do |acc, el|
+    acc + el
+  end
+end
+
+p union(%w[a b], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
+p union(%w[x y], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
